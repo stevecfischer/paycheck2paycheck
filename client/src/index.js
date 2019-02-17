d9cdcffbc91f59412399f6362a8b889241881e5b
Steve <sfischer@redventures.com>
2019-02-16 22:01:16 -0500

    removed unused code

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import HomePage from './containers/HomePage/'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
